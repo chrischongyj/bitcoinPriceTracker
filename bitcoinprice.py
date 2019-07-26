4d6d1c21a483c037ba12a10d1cc789cc4ac06579
chrischongyj <chrischongyj@gmail.com>
2019-07-26 21:56:43 +0800

    Updated price scraping; Added price refresh timer

--- bitcoinprice.py
+++ bitcoinprice.py
@@ -1,15 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
-link = "https://coinmarketcap.com/currencies/bitcoin/"
+def check_price():
+    link = "https://coinmarketcap.com/currencies/bitcoin/"
 
-header = {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'}
+    header = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'}
 
-data = requests.get(headers = header, url = link)
+    data = requests.get(headers = header, url = link)
 
-soup = BeautifulSoup(data.content, 'html.parser')
+    soup = BeautifulSoup(data.content, 'html.parser')
 
-bitcoinPrice = soup.find(id="data-currency-value")
+    bitcoinPrice = soup.find(id="quote_price").span.contents[0]
+    
+    print(bitcoinPrice)
 
-print(bitcoinPrice)
+while(True):
+    check_price()
+    print("Price will refresh again in 5 seconds...")
+    time.sleep(5)
